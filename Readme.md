f8a336cc39c332e5db1c1a018dc3dc318662d57e
luongtt <luongheroes@gmail.com>
2022-05-18 11:25:36 +0700

    update

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Getting Started
+
+### Reference Documentation
+
+- Create database/schema MySQL test
+- Setup docker
+- Run docker-compose.yml: docker-compose up -d
+- Run App normal
+
+>>>>>>>>HAPPY
--- src/test/java/com/sample/example/SampleApplicationTests.java
+++ src/test/java/com/sample/example/SampleApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LuongttVmoApplicationTests {
+class SampleApplicationTests {
 
     @Test
     void contextLoads() {
