fef2a49a4088428d2253686e4b119eabf3056316
luongtt <luongheroes@gmail.com>
2022-05-27 16:03:47 +0700

    update

--- src/main/java/com/sample/example/model/DataSample.java
+++ src/main/java/com/sample/example/model/DataSample.java
@@ -1,10 +1,16 @@
 package com.sample.example.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DataSample {
+    @JsonProperty("data")
     private String data;
 }
